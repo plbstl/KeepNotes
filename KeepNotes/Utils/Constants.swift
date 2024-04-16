6312342e97cada483aaad5f8f202034f18f7d4fc
Paul Ebose <paulebose@gmail.com>
2024-04-16 09:03:23 +0100

    allow creation of new categories

---
+++ KeepNotes/Utils/Constants.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+enum Constants {
+
+   enum Tag {
+      static let all = "All Notes"
+      static let favorite = "Favorite Notes"
+   }
+
+   enum AppInfo {
+      static let name = "Keep Notes"
+   }
+
+}
--- KeepNotes/Views/Home.swift
+++ KeepNotes/Views/Home.swift
@@ -1,11 +1,5 @@
+import SwiftData
 import SwiftUI
-
-// MARK: - DefaultTag
-
-enum DefaultTag: String {
-   case all = "All Notes"
-   case fav = "Favorite Notes"
-}
 
 // MARK: - Home
 
@@ -14,39 +8,71 @@ struct Home: View {
    // MARK: Internal
 
    var body: some View {
+
       NavigationSplitView {
-
          List(selection: $selectedTag) {
-
             Text("All")
-               .tag(DefaultTag.all.rawValue)
-               .foregroundStyle(selectedTag == DefaultTag.all.rawValue ? .primary : .secondary)
+               .tag(Constants.Tag.all)
+               .foregroundStyle(selectedTag == Constants.Tag.all ? .primary : .secondary)
 
             Text("Favorites")
-               .tag(DefaultTag.fav.rawValue)
-               .foregroundStyle(selectedTag == DefaultTag.fav.rawValue ? .primary : .secondary)
+               .tag(Constants.Tag.favorite)
+               .foregroundStyle(selectedTag == Constants.Tag.favorite ? .primary : .secondary)
 
             // User created categories
-            Section {} header: {
+            Section {
+               ForEach(categories) { category in
+                  Text(category.title)
+                     .tag(category.title)
+                     .foregroundStyle(selectedTag == category.title ? .primary : .secondary)
+               }
+            } header: {
                HStack(spacing: 6) {
                   Text("Categories")
 
-                  Button("", systemImage: "plus") {}
-                     .help("Create a new category")
-                     .buttonStyle(.plain)
-                     .tint(.gray)
-                     .offset(y: -2)
+                  Button("", systemImage: "plus") {
+                     shouldCreateNewCategory = true
+                  }
+                  .help("Create a new category")
+                  .buttonStyle(.plain)
+                  .tint(.gray)
+                  .offset(y: -2)
                }
             }
-
          }
 
       } detail: {}
-         .navigationTitle(selectedTag ?? "$(PRODUCT_NAME)")
+         .navigationTitle(selectedTag ?? Constants.AppInfo.name)
+         .alert("Create a new category", isPresented: $shouldCreateNewCategory) {
+            TextField("Category name", text: $newCategoryTitle)
+
+            Button("Create") {
+               // Do not create a category with an empty title
+               guard !newCategoryTitle.isEmpty else {
+                  return
+               }
+
+               // Create new category and add to modelContext
+               let category = NoteCategory(title: newCategoryTitle)
+               modelContext.insert(category)
+               newCategoryTitle = ""
+            }
+
+            Button("Cancel", role: .cancel) {
+               // Do nothing. For convenience, do not reset newCategoryTitle
+            }
+
+         }
    }
 
    // MARK: Private
 
+   @Environment(\.modelContext) private var modelContext
+   @Query(animation: .bouncy) private var categories: [NoteCategory]
+   /// Whether to show an alert for creating a new category in the NavigationSplitView
+   @State private var shouldCreateNewCategory = false
+   /// this is used when creating a new category
+   @State private var newCategoryTitle = ""
    /// This is used to filter the selected note category
    @State private var selectedTag: String? = "All Notes"
 
